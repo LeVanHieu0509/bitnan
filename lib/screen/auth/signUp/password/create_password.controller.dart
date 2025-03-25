5d040e3e9b2f92bd009491ed027a7bf198f2d572
LeVanHieu0509 <levanhieu.huflit@gmail.com>
2025-03-25 14:31:18 +0700

    create pass

--- lib/screen/auth/signUp/password/create_password.controller.dart
+++ lib/screen/auth/signUp/password/create_password.controller.dart
@@ -12,16 +12,34 @@ import 'package:bitnan/@share/utils/util.dart';
 import 'package:bitnan/resource/image.resource.dart';
 import 'package:bitnan/resource/style.resource.dart';
 
+/*
+Đoạn mã trên là controller cho màn hình tạo mật khẩu trong ứng dụng Flutter. 
+Controller này sử dụng GetX để quản lý trạng thái và xử lý các sự kiện 
+liên quan đến việc tạo mật khẩu (passcode) mới của người dùng khi đăng ký tài khoản.
+ */
 class CreatePasswordController extends GetxController {
+  // Đây là đối tượng chứa thông tin đăng ký của người dùng, được lấy từ getArgument().
+  // Đối tượng này sẽ bao gồm các trường như email, tên đầy đủ, và mật khẩu (passcode).
   late final SignUpRequest signUpRequest;
-  var passWord = ''.obs;
+  var passWord =
+      ''.obs; // Các biến này là reactive variables (Rx<String>), lưu trữ mật khẩu và xác nhận mật khẩu của người dùng.
   var confirmPass = ''.obs;
-  get ready => passWord.value.length == 6 && confirmPass.value.length == 6;
-  get matched => passWord.value == confirmPass.value;
+  get ready =>
+      passWord.value.length == 6 &&
+      confirmPass.value.length ==
+          6; // Trả về true nếu cả mật khẩu và xác nhận mật khẩu đều có độ dài bằng 6 ký tự (điều kiện để mật khẩu hợp lệ).
+  get matched =>
+      passWord.value ==
+      confirmPass
+          .value; // Trả về true nếu mật khẩu và xác nhận mật khẩu trùng nhau
 
+  // Các đối tượng để truy cập vào UserRepo và DataStorage.
+  // UserRepo giúp giao tiếp với API, còn DataStorage lưu trữ thông tin người dùng như token và tên.
   var userRepo = Get.find<UserRepo>();
   var store = Get.find<DataStorage>();
 
+  // Các đối tượng FocusNode và TextEditingController dùng để quản lý trường nhập mật khẩu
+  // và xác nhận mật khẩu, giúp điều khiển giao diện và xử lý sự kiện người dùng nhập liệu.
   final passwordNode = FocusNode();
   final passwordEditingController = TextEditingController();
   final passwordConfirmNode = FocusNode();
@@ -30,23 +48,63 @@ class CreatePasswordController extends GetxController {
   @override
   onInit() {
     super.onInit();
+    // Phương thức này được gọi khi CreatePasswordController được khởi tạo.
+    // Tại đây, đối tượng signUpRequest được lấy từ đối tượng đã được truyền vào controller thông qua getArgument().
     signUpRequest = getArgument();
   }
 
+  // Phương thức này xóa mật khẩu và xác nhận mật khẩu.
+  // Đây là một phần của các hành động khi người dùng nhập mật khẩu không hợp lệ và cần nhập lại.
   _clearAllPass() {
     passWord.value = '';
     confirmPass.value = '';
   }
 
+  // Phương thức này được gọi khi người dùng nhấn nút xác nhận để tạo mật khẩu
+  Future<void> handleSubmit() async {
+    // Nếu mật khẩu hoặc xác nhận mật khẩu không hợp lệ (nghĩa là không đủ dài hoặc không trùng khớp),
+    // sẽ hiển thị modal thông báo yêu cầu người dùng nhập lại.
+    if (!ready) {
+      return await showModalSheet(
+        url: MyImage.ic_lock,
+        title: kInvalidNumberPass,
+        message: kInputAgain,
+        action: _clearAllPass,
+      );
+    }
+
+    if (!matched) {
+      return await showModalSheet(
+        url: MyImage.ic_lock,
+        title: kInvalidPass,
+        message: kInputAgain,
+        action: _clearAllPass,
+      );
+    }
+
+    //  // Nếu mật khẩu hợp lệ, phương thức sẽ gọi _submitPass() để gửi yêu cầu đăng ký tới server.
+    return await _submitPass();
+  }
+
+  // Phương thức này thực hiện việc gửi yêu cầu đăng ký tới server
   Future _submitPass() async {
     showLoading();
+
+    // Đặt mật khẩu vào signUpRequest
     signUpRequest.passcode = passWord.value;
+
+    // Gọi userRepo.signUp() để thực hiện yêu cầu đăng ký với thông tin người dùng.
     var res = await userRepo.signUp(request: signUpRequest);
     hideLoading();
+
+    // Nếu đăng ký thành công (mã trả về là kSuccessApi),
+    // thông tin người dùng sẽ được lưu vào DataStorage
+    // và người dùng sẽ được chuyển đến màn hình đăng nhập với thông báo thành công.
     if (res.status == kSuccessApi) {
       await store.setFullName(signUpRequest.fullName);
       await store.setToken(res.data);
       await userRepo.getProfile().then((value) => store.setUser(value.data));
+
       showModalSheet(
         dismissible: false,
         url: MyImage.ic_account_success,
@@ -61,6 +119,7 @@ class CreatePasswordController extends GetxController {
         message: kWellComeMember,
       );
     } else {
+      // Nếu có lỗi, ví dụ như token hết hạn, sẽ hiển thị modal cảnh báo và yêu cầu người dùng thử lại.
       if (res.getErrors() == TOKEN_INVALID) {
         showModalSheet(
           title: kTitleExpired,
@@ -74,25 +133,19 @@ class CreatePasswordController extends GetxController {
       }
     }
   }
-
-  Future<void> handleSubmit() async {
-    if (!ready) {
-      return await showModalSheet(
-        url: MyImage.ic_lock,
-        title: kInvalidNumberPass,
-        message: kInputAgain,
-        action: _clearAllPass,
-      );
-    }
-    if (!matched) {
-      return await showModalSheet(
-        url: MyImage.ic_lock,
-        title: kInvalidPass,
-        message: kInputAgain,
-        action: _clearAllPass,
-      );
-    }
-
-    return await _submitPass();
-  }
 }
+
+
+/*
+Tóm tắt về các phương thức và chức năng trong CreatePasswordController:
+1. onInit(): Khởi tạo controller và lấy thông tin đăng ký từ màn hình trước.
+2. _clearAllPass(): Xóa mật khẩu và xác nhận mật khẩu.
+3. _submitPass(): Gửi yêu cầu đăng ký tới API và xử lý kết quả đăng ký.
+4. handleSubmit(): Kiểm tra tính hợp lệ của mật khẩu và gọi phương thức _submitPass() nếu hợp lệ.
+5. ready: Trả về true nếu cả mật khẩu và xác nhận mật khẩu đều đủ dài.
+6. matched: Trả về true nếu mật khẩu và xác nhận mật khẩu khớp nhau.
+7. Tính năng chính của CreatePasswordController:
+8. Kiểm tra mật khẩu người dùng nhập vào và đảm bảo rằng chúng hợp lệ (đủ dài và khớp nhau).
+9. Gửi yêu cầu đăng ký tới server khi mật khẩu hợp lệ.
+10. Hiển thị thông báo khi có lỗi hoặc yêu cầu người dùng nhập lại mật khẩu.
+ */
--- lib/screen/auth/signUp/password/create_password.screen.dart
+++ lib/screen/auth/signUp/password/create_password.screen.dart
@@ -12,7 +12,12 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:bitnan/screen/auth/signUp/password/create_password.controller.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+/*
+  CreatePasswordScreen kế thừa từ GetWidget<CreatePasswordController>. 
+  Điều này giúp widget tự động liên kết với CreatePasswordController mà không cần phải gọi Get.find().
+ */
 class CreatePasswordScreen extends GetWidget<CreatePasswordController> {
+  // formKey là khóa duy trì trạng thái của form, giúp xác thực các trường nhập liệu
   final formKey = GlobalKey<FormState>();
 
   CreatePasswordScreen({Key? key}) : super(key: key);
@@ -20,15 +25,18 @@ class CreatePasswordScreen extends GetWidget<CreatePasswordController> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      isShowLeading: true,
-      appBarColor: MyColor.white,
+      isShowLeading:
+          true, // Điều khiển xem có hiển thị nút quay lại (back button) hay không
+      appBarColor: MyColor.white, // Màu nền cho AppBar
       body: Form(
-        key: formKey,
+        key:
+            formKey, // Dùng để nhóm các trường nhập liệu lại với nhau, và kiểm tra tính hợp lệ của chúng
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 24.h),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // Hiển thị các văn bản yêu cầu người dùng nhập mật khẩu và xác nhận mật khẩu
               Text(
                 getLocalize(kCreatePass),
                 style: MyStyle.typeBold.copyWith(
@@ -54,6 +62,7 @@ class CreatePasswordScreen extends GetWidget<CreatePasswordController> {
                 ),
               ),
               16.h.heightBox,
+              //  Hiển thị trường nhập mật khẩu đầu tiên
               _passwordInput(context),
               32.h.heightBox,
               Text(
@@ -64,8 +73,10 @@ class CreatePasswordScreen extends GetWidget<CreatePasswordController> {
                 ),
               ),
               16.h.heightBox,
+              // Hiển thị trường nhập lại mật khẩu để xác nhận
               passwordConfirmInput(context),
               const Spacer(),
+              // Nút "Xác nhận" để người dùng gửi mật khẩu
               SubmitButton2(text: kConfirm, action: controller.handleSubmit),
               // _itemNumericKeyboard(),
             ],
@@ -75,19 +86,30 @@ class CreatePasswordScreen extends GetWidget<CreatePasswordController> {
     );
   }
 
+  // là một widget tùy chỉnh dùng để nhập mật khẩu,
+  // có thể điều chỉnh các thuộc tính như controller, onChanged, onCompleted.
   Widget _passwordInput(BuildContext context) => BBPasswordInput(
     appContext: context,
-    autoFocus: true,
+    autoFocus:
+        true, // Trường mật khẩu đầu tiên sẽ tự động lấy tiêu điểm khi màn hình được mở
     controller: controller.passwordEditingController,
-    onChanged: (v) => controller.passWord.value = v,
+    onChanged:
+        (v) =>
+            controller.passWord.value =
+                v, // Mỗi khi người dùng thay đổi mật khẩu, giá trị mới sẽ được gán vào controller.passWord.
     onCompleted: (v) => controller.passwordConfirmNode.requestFocus(),
   );
 
+  // passwordConfirmInput: Tương tự như _passwordInput,
+  // nhưng đây là trường để người dùng xác nhận mật khẩu đã nhập.
   Widget passwordConfirmInput(BuildContext context) => BBPasswordInput(
     appContext: context,
     focusNode: controller.passwordConfirmNode,
     controller: controller.passwordConfirmEditingController,
     onChanged: (v) => controller.confirmPass.value = v,
-    onCompleted: (v) => controller.handleSubmit(),
+    onCompleted:
+        (v) =>
+            controller
+                .handleSubmit(), //Khi người dùng nhập xong mật khẩu và nhấn "xác nhận", phương thức controller.handleSubmit() sẽ được gọi để xử lý.
   );
 }
